2254571ca8f010c3f5421c6e853d1e0a2d8bcd6e
andybelyx <andy_belyx@ukr.net>
2019-07-26 10:28:25 +0300

    migration

--- cart/app/Http/Resources/ProductVariationResource.php
+++ cart/app/Http/Resources/ProductVariationResource.php
@@ -24,6 +24,9 @@ class ProductVariationResource extends JsonResource
             'name' => $this->name,
             'price' => $this->formattedPrice,
             'price_varies' => $this->priceVaries(),
+            'stock_count' => (int) $this->stockCount(),
+            'in_stock' => $this->inStock(),
+
         ];
     }
 }
--- cart/database/migrations/2019_07_25_201100_create_product_variation_stock_view.php
+++ cart/database/migrations/2019_07_25_201100_create_product_variation_stock_view.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
+
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateProductVariationStockView extends Migration
 {
@@ -19,25 +19,25 @@ class CreateProductVariationStockView extends Migration
                 product_variations.product_id AS product_id,
                 product_variations.id AS product_variation_id,
                 COALESCE(SUM(stocks.quantity) - COALESCE(SUM(product_variation_order.quantity), 0), 0) AS stock,
-                CASE WHEN COALESCE(SUM(stocks.quantity) - COALESCE(SUM(product_variation_order.quantity), 0), 0) > 0
-                    then TRUE
-                    else FALSE
+                case when COALESCE(SUM(stocks.quantity) - COALESCE(SUM(product_variation_order.quantity), 0), 0) > 0
+                    then true
+                    else false
                 end in_stock
             FROM product_variations
             LEFT JOIN (
-            SELECT 
-                stocks.product_variation_id AS id,
-                SUM(stocks.quantity) AS quantity
-            FROM stocks
-            GROUP BY stocks.product_variation_id
-            ) AS stocks USING (id)
+                SELECT
+                    stocks.product_variation_id AS id,
+                    SUM(stocks.quantity) AS quantity
+                FROM stocks
+                GROUP BY stocks.product_variation_id
+                ) AS stocks USING (id)
             LEFT JOIN (
-            SELECT
-                product_variation_order.product_variation_id AS id,
-                SUM(product_variation_order.quantity) AS quantity
+                SELECT
+                    product_variation_order.product_variation_id AS id,
+                    SUM(product_variation_order.quantity) AS quantity
                 FROM product_variation_order
                 GROUP BY product_variation_order.product_variation_id
-                ) AS product_variation_order USING (id)
+            ) AS product_variation_order USING (id)
             GROUP BY product_variations.id
         ");
     }
@@ -49,6 +49,6 @@ class CreateProductVariationStockView extends Migration
      */
     public function down()
     {
-        \DB::statement('DROP VIEW IF EXISTS product_variation_stock_view');
+        DB::statement('DROP VIEW IF EXISTS product_variation_stock_view');
     }
 }
